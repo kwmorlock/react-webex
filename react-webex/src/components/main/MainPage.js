c85624480b3eafbcd63e0e79eeca171617864c00
kwmorlock <kmorlockcodes@gmail.com>
2021-02-17 12:18:45 -0500

    set: MainPage.js MainPage

--- react-webex/src/components/main/MainPage.js
+++ react-webex/src/components/main/MainPage.js
@@ -1,1 +1,13 @@
-//setup
+import React from "react";
+
+const MainPage = () => {
+  return (
+    <>
+      <div>
+        <h1>Welcome to Webex Demo!</h1>
+      </div>
+    </>
+  );
+};
+
+export default MainPage;
