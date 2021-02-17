12b2122eca6f35972efd0b461d0e3226068ab32e
kwmorlock <kmorlockcodes@gmail.com>
2021-02-17 14:14:51 -0500

    set: axiosWithAuth.js token

--- react-webex/src/utils/axiosWithAuth.js
+++ react-webex/src/utils/axiosWithAuth.js
@@ -1,1 +1,12 @@
-//setup
+import axios from "axios";
+
+export const axiosWithAuth = () => {
+  const token = localStorage.getItem("token");
+
+  return axios.create({
+    baseURL: "http://localhost:5000/",
+    headers: {
+      Authorization: token,
+    },
+  });
+};
