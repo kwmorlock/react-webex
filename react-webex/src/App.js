bca6487d9ad6c3d45693d9fa59bdac263ae7b839
kwmorlock <kmorlockcodes@gmail.com>
2021-02-17 14:23:43 -0500

    set: app.js Register Route

--- react-webex/src/App.js
+++ react-webex/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import MainPage from "./components/main/MainPage";
+import Register from "./components/users/UserReg";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Router>
       <div className="App">
         <Switch>
+          <Route path="/register" component={Register} />
           <Route path="/" component={MainPage} />
         </Switch>
       </div>
